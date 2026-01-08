2b2a7147195a1e023377da4e94f713790a524b8b
Mikkel Malmberg <mikkel@brnbw.com>
2026-01-08 15:20:42 +0100

    Try again, update prettier config, format

--- test/index.test.js
+++ test/index.test.js
@@ -38,10 +38,7 @@ test('crasher uncaught exception', async function (t) {
     return logContent.includes('Test uncaught exception')
   })
   const logContent = await fs.promises.readFile(crashlogPath, 'utf8')
-  t.ok(
-    logContent.includes('Test uncaught exception'),
-    'Log should contain the error message'
-  )
+  t.ok(logContent.includes('Test uncaught exception'), 'Log should contain the error message')
 })
 
 test('crasher unhandled rejection', async function (t) {
@@ -72,8 +69,5 @@ test('crasher unhandled rejection', async function (t) {
     return logContent.includes('Test unhandled rejection')
   })
   const logContent = await fs.promises.readFile(crashlogPath, 'utf8')
-  t.ok(
-    logContent.includes('Test unhandled rejection'),
-    'Log should contain the error message'
-  )
+  t.ok(logContent.includes('Test unhandled rejection'), 'Log should contain the error message')
 })
